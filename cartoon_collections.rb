3955622b33a68f88379c508239129409cc244257
Kara Odle <karajo.odle@gmail.com>
2019-08-26 18:13:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,14 +7,7 @@ def summon_captain_planet(array)
   array.map { |word| word.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
 
 
 def long_planeteer_calls(array)
