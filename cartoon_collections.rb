e7a14710281388739b97e354df88e4e0218895f4
Kara Odle <karajo.odle@gmail.com>
2019-08-26 18:23:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,17 +25,17 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array = []
+  new_array = []
   
   array.each do |item|
     if cheese_types.include?(item)
-      array << item 
+      new_array << item 
     end
   end 
   
   binding.pry
   
-  array.length > 0 ? array[0] : nil
+  new_array.length > 0 ? new_array[0] : nil
   
 end
 
