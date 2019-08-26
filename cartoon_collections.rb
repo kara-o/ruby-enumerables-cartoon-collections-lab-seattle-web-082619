15ccab49a06905723ee8f823c017035d01bfb9a4
Kara Odle <karajo.odle@gmail.com>
2019-08-26 18:20:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,17 @@ def long_planeteer_calls(array)
   new_arr.length > 0 ? true : false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array = []
+  
+  array.each do |item|
+    if cheese_types.include?(item)
+      array << item 
+    end
+  end 
+  
+  array.length > 0 ? array[0] : nil
+  
 end
