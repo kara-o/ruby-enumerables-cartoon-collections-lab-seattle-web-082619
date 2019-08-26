716b59a4dc80b175a47101265aa0421e7f60da6e
Kara Odle <karajo.odle@gmail.com>
2019-08-26 18:05:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,10 +2,6 @@ def roll_call_dwarves(array)
   array.each_with_index { |item, index| puts "#{index + 1}. #{item}" }
 end
 
-def summon_captain_planet
-  def roll_call_dwarves(array)
-  array.each_with_index { |item, index| puts "#{index + 1}. #{item}" }
-end
 
 def summon_captain_planet(array)
   array.map { |word| word.capitalize + "!" }
